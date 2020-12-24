f9ae9287f56fd703e24574ec81c55f94ef8f8c2f
tallninja <tallninja123@gmail.com>
2020-12-24 15:02:56 +0300

    size of

---
+++ SizeOf/src/SizeOf.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+
+int main()
+{
+  printf("\nSize of Char: %ld Bytes\n", sizeof(char));
+  printf("Size of Short: %ld Bytes\n", sizeof(short));
+  printf("Size of Integer: %ld Bytes\n", sizeof(int));
+  printf("Size of Long %ld Bytes\n", sizeof(long));
+  printf("Size of Long Long: %ld Bytes\n", sizeof(long long));
+  printf("Size of Double: %ld Bytes\n", sizeof(double));
+  printf("Size of Long Double: %ld Bytes\n", sizeof(long double));
+  printf("Size of Float: %ld Bytes\n\n", sizeof(float));
+  return 0;
+}
