8748773f637fd51c8b5bb27c3174c0fccb9ac951
tallninja <tallninja123@gmail.com>
2020-12-29 12:56:45 +0300

    string reversal

---
+++ StringReversal/src/main.c
@@ -0,0 +1,42 @@
+/*
+ * Author: Ernest Wambua
+ * Date: 29/12/20
+ * Purpose: Reversing a string using pointers
+ * 
+ */
+ 
+# include <stdio.h>
+# include <stdlib.h>
+# include <stddef.h>
+# include <string.h>
+ 
+
+void reverse(char* string, int stringLength, char* reversed);
+
+int main(int argc, char **argv)
+{
+	char name[] = "Ernest";
+	char* reversed = NULL;
+	reversed = (char *)malloc(20 * sizeof(char));
+	
+	if (reversed != NULL)
+	{
+		reverse(name, strlen(name), reversed);
+	}
+	printf("string = %s\n", name);
+	printf("reversed = %s\n", reversed);
+	
+	free(reversed);
+	reversed = NULL;
+}
+
+
+void reverse(char* string, int stringLength, char* reversed)
+{
+	char* stringEnd = string + (stringLength - 1); // get to the last character excluding the null character
+	
+	while (stringEnd >= string)
+	{
+		*reversed++ = *stringEnd--;
+	}
+}
